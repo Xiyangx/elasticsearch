74b0f62aeb87b6a109e4a275168f126d757527dc
xiyang <13263418333@163.co>
2022-06-20 15:31:44 +0800

    部署版本v1.0

--- src/main/java/com/sunac/elasticsearch/controllar/DownloadController.java
+++ src/main/java/com/sunac/elasticsearch/controllar/DownloadController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 /**
  * @Description: TODO
@@ -25,25 +26,44 @@ public class DownloadController {
     @RequestMapping(value = "/download/{year}/{month}", method = RequestMethod.GET)
     public ResponseEntity<InputStreamResource> downloadFile(@PathVariable String year, @PathVariable String month) throws IOException {
 
-
-
 //        String fileName = "/Users/xiyang/Documents/sunac/文档/项目/序时账/excel/"
 //                  + year
 //                  + "/"
 //                  + month
 //                  + ".zip";
-        String fileName = "/opt/project/file/"
+        String filePath = "/opt/project/file/"
                 + year
-                + "/"
-                + month
-                + ".zip";
+                + "/";
+        String fileName = month + ".zip";
 
-        FileSystemResource file = new FileSystemResource(fileName);
         logger.info("--------要下载的文件是：{}/{}.zip------",year,month);
+
+        return getInputStreamResourceResponseEntity(filePath, fileName);
+
+    }
+
+    @RequestMapping(value = "/download/{year}/{month}/{company}", method = RequestMethod.GET)
+    public ResponseEntity<InputStreamResource> downloadFile(@PathVariable String year, @PathVariable String month,@PathVariable String company) throws IOException {
+
+        String filePath = "/Users/xiyang/Documents/sunac/文档/项目/序时账/excel/"
+                  + year
+                  + "/"
+                  + month
+                  + "/";
+        String fileName = company + ".xlsx";
+        logger.info("--------要下载的文件是：{}/{}/{}.xlsx------",year,month,company);
+
+
+        return getInputStreamResourceResponseEntity(filePath, fileName);
+
+    }
+
+    private ResponseEntity<InputStreamResource> getInputStreamResourceResponseEntity(String filePath, String fileName) throws IOException {
+        FileSystemResource file = new FileSystemResource(filePath + fileName);
         logger.info("--------正在下载------");
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getFilename()));
+        headers.add("content-disposition","attachment;filename="+ URLEncoder.encode(fileName,"utf-8"));
         headers.add("Pragma", "no-cache");
         headers.add("Expires", "0");
 
@@ -51,9 +71,7 @@ public class DownloadController {
                 .ok()
                 .headers(headers)
                 .contentLength(file.contentLength())
-                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .contentType(MediaType.parseMediaType("application/octet-stream;charset=utf-8"))
                 .body(new InputStreamResource(file.getInputStream()));
-
     }
-
 }
